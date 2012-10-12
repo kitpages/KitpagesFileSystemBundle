6fb9a2cbdfe4d6d4cf05dfd56e5126bb518be70b
Sebastien Lefebvre - Kitpages <sebastien.lefebvre@kitpages.fr>
2012-10-12 15:20:02 +0200

    RC

--- Service/Adapter/Ftp.php
+++ Service/Adapter/Ftp.php
@@ -5,7 +5,6 @@ namespace Kitpages\FileSystemBundle\Service\Adapter;
 // external service
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-
 class Ftp implements AdapterInterface{
     ////
     // dependency injection
@@ -19,5 +18,29 @@ class Ftp implements AdapterInterface{
     {
         $this->directory = $directory;
     }
+    /*********************/
+    //action
+    /*********************/
+    function copyTempToAdapter($tempPath, AdapterFileInterface $file){}
 
+    function copyAdapterToTemp(AdapterFileInterface $file, $tempPath){}
+
+    function rename(AdapterFileInterface $tempFile, AdapterFileInterface $targetFile){}
+
+    function unlink(AdapterFileInterface $targetFile){}
+
+    function copy(AdapterFileInterface $targetFile, AdapterFileInterface $targetFileCopy){}
+
+    /*********************/
+    // information
+    /*********************/
+    function isFile(AdapterFileInterface $targetFile){}
+
+    function sendFileToBrowser(AdapterFileInterface $targetFile, $name = null){}
+
+    function getFileContent(AdapterFileInterface $targetFile){}
+
+    function getFileLocation(AdapterFileInterface $targetFile){}
+
+    function rmdirr(AdapterFileInterface $directory){}
 }
